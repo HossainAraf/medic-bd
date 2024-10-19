8c9af17f69f379eaf5f5db11b8647bc0e56cc839
Hossain <arafat.kd99@gmail.com>
2024-10-19 19:27:49 +0600

    Create DoctorSchedules table for available_day & time

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_18_055755) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_19_131425) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "chembers", force: :cascade do |t|
+    t.string "name"
+    t.string "type"
+    t.string "address"
+    t.integer "district_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "districts", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "doctors", id: :serial, force: :cascade do |t|
     t.string "name", limit: 255, null: false
