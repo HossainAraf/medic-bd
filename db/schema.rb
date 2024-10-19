801952af89516077a4788440a4222b0962c60e3b
Hossain <arafat.kd99@gmail.com>
2024-10-19 19:50:22 +0600

    Add contact column to Chembers table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_10_19_131425) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_19_133331) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,16 @@ ActiveRecord::Schema[7.1].define(version: 2024_10_19_131425) do
 
   create_table "districts", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "doctor_schedules", force: :cascade do |t|
+    t.integer "doctor_id"
+    t.integer "district_id"
+    t.integer "chember_id"
+    t.string "available_day"
+    t.string "available_time"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
