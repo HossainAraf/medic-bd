b2820500e7793a391be6d2a05101bae55de3e6c7
Hossain <arafat.kd99@gmail.com>
2024-10-29 16:37:57 +0600

    Add has-many chembers through doctor_schedules for doctors

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,6 +1,7 @@
 class Doctor < ApplicationRecord
   belongs_to :specification
   has_many :doctor_schedules
+  has_many :chembers, through: :doctor_schedules
   Doctor.joins(:specification, doctor_schedule:  :chember)
               .select('doctors.name, chembers.name as chember_name, doctor_schedules.available_day, doctor_schedules.available_time')
               .where('specifications.name: {selected_specification}')
