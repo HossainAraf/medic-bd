5ca926d0b669fb614e7f49ab9e2d40f545baeb4d
Hossain <arafat.kd99@gmail.com>
2024-10-30 18:57:30 +0600

    Add doctors method in SpecializationsControll  to retrieve doctors data along with necessary assoiciations for each specialization id

--- app/controllers/api/v1/specifications_controller.rb
+++ app/controllers/api/v1/specifications_controller.rb
@@ -9,6 +9,22 @@ class Api::V1::SpecificationsController < ApplicationController
     render json: specification
   end
 
+  def doctors
+    # Find doctors with the specified specification_id and include associated data
+    @doctors = Doctor.joins(:specification, doctor_schedule: :chember)
+                                      .where(specialization_id: params[:id])
+                                      .select('doctors*, specializations.name AS specialization_name, chembers.name AS chember_name, doctor_schedule.available_day, doctor_schedule.available_time')
+
+    if @doctors.any?
+      render json:  @doctors.as_json(
+      only: [:id, :name],
+      methods: [:specialization_name, chember_name, available_day, available_time]
+    )
+    else
+      render json:  {error: 'No doctors found with this specified specification'}
+    end
+  end
+
   # endpoint: POST /api/v1/specifications
   def create
     specification = Specification.new(specification_params)
