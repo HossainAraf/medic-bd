e8ae672fae58f9ca41c73263a8ed59630ce5b3e6
Hossain <arafat.kd99@gmail.com>
2024-11-17 11:43:13 +0600

    Fix indention to align with rubocop rules

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,4 +1,6 @@
 class Doctor < ApplicationRecord
   validates :order, presence: true,
-                    numericality: { only_integer: true, greater_than_or_equal_to: 100_000, less_than_or_equal_to: 9_999_999 }
+                    numericality: { only_integer: true,
+                                    greater_than_or_equal_to: 100_000,
+                                    less_than_or_equal_to: 9_999_999 }
 end
