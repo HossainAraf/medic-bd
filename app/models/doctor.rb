57e951f7bf0c8dc5871c8a134e99d1e515e3de4d
Hossain <arafat.kd99@gmail.com>
2024-11-17 11:33:34 +0600

    Validate Doctor model to ensure limit the renge of input

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,2 +1,4 @@
 class Doctor < ApplicationRecord
+  validates :order, presence: true,
+                    numericality: { only_integer: true, greater_than_or_equal_to: 100_000, less_than_or_equal_to: 9_999_999 }
 end
