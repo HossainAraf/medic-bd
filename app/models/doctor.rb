6b70992f2abd69487882d077dc0199f9fbde3d99
Hossain <arafat.kd99@gmail.com>
2024-11-18 19:30:33 +0600

    Update Doctor & Specialization models in order of query each other through DoctorSpecialization model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -4,4 +4,5 @@ class Doctor < ApplicationRecord
                                     greater_than_or_equal_to: 100_000,
                                     less_than_or_equal_to: 9_999_999 }
   has_many :doctor_specializations
+  has_many :specializations, through: :doctor_specializations
 end
