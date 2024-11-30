0b825012c2149a22c6a07cbb78df455a2d34ff6f
Hossain <arafat.kd99@gmail.com>
2024-11-30 20:05:26 +0600

    Update Chambers index method to find chambers filtering districts

--- app/controllers/api/v1/chambers_controller.rb
+++ app/controllers/api/v1/chambers_controller.rb
@@ -1,4 +1,12 @@
 class Api::V1::ChambersController < ApplicationController
   def index
+    if params[:district_id].present?
+      @chambers = Chamber.where(district_id: params[:district_id]).includes(:district)
+    else
+      @chambers = Chamber.all.includes(:district)
+    end
+
+    render json: @chambers.as_json(include: { district: { only: [:name] } })
+    end
   end
 end
