1bf292bddd0a5968e18d7b3174ed0a3038f3c560
Hossain <arafat.kd99@gmail.com>
2024-12-15 01:05:32 +0600

    Fix indention & align other rubocop rules

--- app/controllers/api/v1/specializations_controller.rb
+++ app/controllers/api/v1/specializations_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::SpecializationsController < ApplicationController
   def index
     specializations = Specialization.all
-    specializations = specializations.sort_by { |specialization| specialization.id }
+    specializations = specializations.sort_by(&:id)
     render json: specializations
   end
 
