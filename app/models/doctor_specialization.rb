0e6469a25ac7b49b0425b108c9da92afafb4026a
Hossain <arafat.kd99@gmail.com>
2025-01-16 04:51:01 +0600

    Add uniqeness validation in DoctorSpecializaion model

--- app/models/doctor_specialization.rb
+++ app/models/doctor_specialization.rb
@@ -1,4 +1,6 @@
 class DoctorSpecialization < ApplicationRecord
   belongs_to :doctor
   belongs_to :specialization
+
+  validates :doctor_id, uniqueness: { scope: :specialization_id, message: 'already has this specialization' }
 end
