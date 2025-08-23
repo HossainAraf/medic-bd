40ee1442b9f61be2cff354b024469fa713bccb83
HossainAraf <arafat.kd99@gmail.com>
2025-08-23 13:57:38 +0600

    Add special note and description column in doctors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_08_23_073240) do
+ActiveRecord::Schema[7.1].define(version: 2025_08_23_075625) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -64,6 +64,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_08_23_073240) do
     t.integer "order", null: false
     t.string "bangla_name"
     t.text "photo_url"
+    t.text "special_notes"
+    t.text "description"
     t.check_constraint "\"order\" >= 100000 AND \"order\" <= 9999999", name: "order_range"
   end
 
