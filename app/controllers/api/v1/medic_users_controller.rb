80cc9d5a1b5a692033823e568cef02dc37c7596b
HossainAraf <arafat.kd99@gmail.com>
2025-08-27 02:23:44 +0600

    update(medic_user_controller) to POST(create:signup) and GET users

--- app/controllers/api/v1/medic_users_controller.rb
+++ app/controllers/api/v1/medic_users_controller.rb
@@ -1,2 +1,31 @@
 class Api::V1::MedicUsersController < ApplicationController
+  skip_before_action :authorize_request, only: [:create]
+
+  # POST /api/v1/medic_users
+  def create
+    user = MedicUser.new(medic_user_params)
+    if user.save
+      token = JsonWebToken.encode(user_id: user.id)
+      render json: { token, user: user_response(user) }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # GET /api/v1/medic_users
+  def index
+    users = MedicUser.all
+    render json: users.map { |user| user_response(user) }, status: :ok
+  end
+
+  private
+
+  def user_params
+    params.require(:medic_user).permit(:name, :email, :phone, :password,, :password_confiramtion, :role)
+  end
+
+  def user_response(user)
+    user.as_json(only: [:id, :name, :email, :phone, :role, :created_at, :updated_at])
+  end
+
 end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -8,7 +8,8 @@ module Api
         user = MedicUser.find_by(email: params[:email]&.downcase)
         if user&.authenticate(params[:password])
           token = JsonWebToken.encode(user_id: user.id)
-          render json: { token:, user: { id: user.id, name: user.name, email: user.email, role: user.role } }, status: :ok
+          render json: { token:, user: { id: user.id, name: user.name, email: user.email, role: user.role } },
+                 status: :ok
         else
           render json: { error: 'Invalid email or password' }, status: :unauthorized
         end
