8b8501806e94b19c19956992ee6cec982755f403
HossainAraf <arafat.kd99@gmail.com>
2025-09-06 02:16:11 +0600

    Rollback last 2 migration and run a new migration foor clearner migration for same purpose

--- db/migrate/20250905181037_prevent_duplicate_doctors.rb
+++
@@ -1,8 +0,0 @@
-class PreventDuplicateDoctors < ActiveRecord::Migration[7.1]
-  def change
-     # Add a unique index on the combination of name and bangla_name.
-    # Prevent the creation of duplicate doctor records with the same name and bangla_name.
-    add_index :doctors, [:name, :bangla_name], unique: true, name: 'index_doctors_on_name_and_bangla_name_unique'
-  end
-end
-1
