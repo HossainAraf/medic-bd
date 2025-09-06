73c47ab65ab9ccc286694f16698f5a8654de3c61
HossainAraf <arafat.kd99@gmail.com>
2025-09-07 00:17:28 +0600

    add authenticatin at the application controller which was temporarily disabled

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,27 @@
 #  ***Please KEEP this file EMPTY to avoid ACCIDENTAL seed data load in production***
+{
+  "doctor": {
+    "name": "Tasnia Habib",
+    "bangla_name": "তাসনিয়া হাবিব সিনথি",
+    "specialty": "স্কেলিং, ফিলিং, রুট ক্যানেল, দাঁত বাঁধাই ও অন্যান্য",
+    "qualification": "BDS, MPhil, PHD",
+    "experience": "সহকারী অধ্যাপক, উদয়ন ডেন্টাল কলেজ, রাজশাহী",
+    "order": 1800005,
+    "photo_url": "",
+    "special_notes": "প্রতি মাসের ১ম ও শেষ শুক্রবার ফ্রি চিকিৎসা সেবা দেয়া হয়",
+    "description": "",
+    "doctor_schedules_attributes": [
+      {
+        "available_day": "প্রতিদিন",
+        "available_time": "বিকাল ৪ টা থেকে রাত ৮ টা",
+        "contact": "01728-174202",
+        "chamber_attributes": {
+          "name": "শুভ ক্লিনিক",
+          "category": "Clinic",
+          "address": "চক এনায়েত, দয়ালের মোড়, নওগাঁ",
+          "district_id": 1
+        }
+      }
+    ]
+  }
+}
