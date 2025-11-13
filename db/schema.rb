b1aed077b6a34f67de602878f1d64f823442f76c
HossainAraf <arafat.kd99@gmail.com>
2025-11-14 03:41:21 +0600

    add md_ prefix to track in shared database

--- db/schema.rb
+++ db/schema.rb
@@ -1,41 +1,52 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_09_09_194233) do
+ActiveRecord::Schema[8.1].define(version: 2025_09_09_194233) do
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension "pg_catalog.plpgsql"
 
   create_table "chambers", force: :cascade do |t|
-    t.string "name"
-    t.string "category"
     t.string "address"
-    t.bigint "district_id", null: false
+    t.string "category"
     t.datetime "created_at", null: false
+    t.bigint "district_id", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
     t.index ["district_id"], name: "index_chambers_on_district_id"
     t.index ["name", "district_id"], name: "index_chambers_on_name_and_district_id", unique: true
   end
 
   create_table "districts", force: :cascade do |t|
-    t.string "name"
     t.datetime "created_at", null: false
+    t.string "name"
     t.datetime "updated_at", null: false
   end
 
   create_table "doctor_schedules", force: :cascade do |t|
     t.string "available_day"
     t.string "available_time"
-    t.bigint "doctor_id", null: false
     t.bigint "chamber_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.string "contact", null: false
+    t.datetime "created_at", null: false
+    t.bigint "doctor_id", null: false
+    t.datetime "updated_at", null: false
     t.index ["chamber_id"], name: "index_doctor_schedules_on_chamber_id"
     t.index ["doctor_id"], name: "index_doctor_schedules_on_doctor_id"
   end
 
   create_table "doctor_specializations", force: :cascade do |t|
-    t.bigint "specialization_id", null: false
-    t.bigint "doctor_id", null: false
     t.datetime "created_at", null: false
+    t.bigint "doctor_id", null: false
+    t.bigint "specialization_id", null: false
     t.datetime "updated_at", null: false
     t.index ["doctor_id", "specialization_id"], name: "unique_doctor_specializations", unique: true
     t.index ["doctor_id"], name: "index_doctor_specializations_on_doctor_id"
@@ -43,38 +54,38 @@ ActiveRecord::Schema[7.1].define(version: 2025_09_09_194233) do
   end
 
   create_table "doctors", force: :cascade do |t|
-    t.string "name", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "specialty", null: false
-    t.string "qualification", null: false
-    t.string "experience", null: false
-    t.string "phone"
-    t.integer "order", null: false
     t.string "bangla_name"
-    t.text "photo_url"
-    t.text "special_notes"
+    t.datetime "created_at", null: false
     t.text "description"
+    t.string "experience", null: false
+    t.string "name", null: false
+    t.integer "order", null: false
+    t.string "phone"
+    t.text "photo_url"
+    t.string "qualification", null: false
+    t.text "special_notes"
+    t.string "specialty", null: false
+    t.datetime "updated_at", null: false
     t.index ["name", "bangla_name"], name: "index_doctors_on_name_and_bangla_name_unique", unique: true
     t.check_constraint "\"order\" >= 100000 AND \"order\" <= 9999999", name: "order_range"
   end
 
   create_table "medic_users", force: :cascade do |t|
-    t.string "name"
-    t.string "email"
-    t.string "phone"
-    t.string "password_digest"
-    t.string "role"
-    t.integer "created_by"
-    t.integer "updated_by"
     t.datetime "created_at", null: false
+    t.integer "created_by"
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.string "phone"
+    t.string "role"
     t.datetime "updated_at", null: false
+    t.integer "updated_by"
     t.index ["email"], name: "index_medic_users_on_email", unique: true
   end
 
   create_table "specializations", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", null: false
+    t.string "name", null: false
     t.datetime "updated_at", null: false
   end
 
