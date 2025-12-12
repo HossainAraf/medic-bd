d19bb42cdaefb8ebe459af19150b680e25c6440d
HossainAraf <arafat.kd99@gmail.com>
2025-12-12 21:22:29 +0600

    chore(application_controller): align indention with rubocop rules

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@ class ApplicationController < ActionController::API
   # Public method for admin authorization(used as callback)
   def authorize_admin
     return if @current_user&.role == 'admin'
+
     render json: { error: 'Forbidden: Admins only' }, status: :forbidden
   end
 
@@ -12,20 +13,19 @@ class ApplicationController < ActionController::API
 
   def authorize_request
     @current_user = authenticate_user_from_token
-    
+
     return if @current_user
 
     log_debug('Current user is NIL')
     render json: { error: 'Not Authorized' }, status: :unauthorized
   end
-  
+
   def authenticate_user_from_token
     token = extract_token_from_header
     return nil unless token
 
     payload = decode_token(token)
     MedicUser.find_by(id: payload[:user_id]) if payload
-
   rescue JWT::DecodeError => e
     log_debug("JWT Decode Error: #{e.message}")
     render json: { error: 'Invalid token' }, status: :unauthorized
@@ -41,7 +41,7 @@ class ApplicationController < ActionController::API
     log_debug("Authorization Header: #{header}")
     header&.split&.last
   end
-  
+
   def decode_token(token)
     log_debug("Extracted Token: #{token}")
     payload = JsonWebToken.decode(token)
@@ -51,5 +51,5 @@ class ApplicationController < ActionController::API
 
   def log_debug(message)
     puts "DEBUG: #{message}"
-  end 
+  end
 end
