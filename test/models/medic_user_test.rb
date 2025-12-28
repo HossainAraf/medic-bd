0ba5626ec66335cca9fd9c1a73d3cc569cca2c1a
HossainAraf <arafat.kd99@gmail.com>
2025-12-28 16:46:01 +0600

    test: add MedicUser presence validation test

--- test/models/medic_user_test.rb
+++ test/models/medic_user_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class MedicUserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # --------------------------
+  # Presence Validations
+  # --------------------------
+  test 'is invalid without email' do
+    user = MedicUser.new
+    assert_not user .valid?
+    assert_includes user.errors[:email], "can't be blank"
+  end
+
+  test 'is invalid without password' do
+    user = MedicUser.new
+    assert_not user .valid?
+    assert_includes user.errors[:password], "can't be blank"
+  end
+
+  #
 end
