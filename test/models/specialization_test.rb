b6d657022c986c26d6831e3b318c94b0e85586a9
HossainAraf <arafat.kd99@gmail.com>
2025-12-31 15:40:26 +0600

    test(Specialization.rb): add uniquieness validation

--- test/models/specialization_test.rb
+++ test/models/specialization_test.rb
@@ -9,4 +9,15 @@ class SpecializationTest < ActiveSupport::TestCase
     assert_not specialization.valid?
     assert_includes specialization.errors[:name], "can't be blank"
   end
+
+  # ---------------------
+  # Uniqueness Validations
+  # --------------------
+  test 'is invalid with duplicate name (case insensitive)' do
+    Specialization.create!(name: "Cardiology")
+    duplicate_specialization = Specialization.new(name: "Skin")
+
+    assert_not duplicate_specialization.valid?
+    assert_includes duplicate_specialization.errors[:name], "has already been taken"
+  end
 end
