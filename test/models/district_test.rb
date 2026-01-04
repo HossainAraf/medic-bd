39639b077d4df539c6f204d6eeb7f2e263c34d2a
HossainAraf <arafat.kd99@gmail.com>
2026-01-04 18:14:06 +0600

    test(District): uniqness validation

--- test/models/district_test.rb
+++ test/models/district_test.rb
@@ -10,4 +10,14 @@ class DistrictTest < ActiveSupport::TestCase
     assert_includes district.errors[:name], "can't be blank"
   end
 
+  # ---------------------
+  # Uniqueness Validations
+  # ---------------------
+  test 'is invalid with duplicate name (case insensitive)' do
+    district = District.create!(name: 'Dhaka')
+    duplicate_district = District.new(name: 'dhaka')
+
+    assert_not duplicate_district.valid?
+    assert_includes duplicate_district.errors[:name], 'has already been taken'
+  end
 end
