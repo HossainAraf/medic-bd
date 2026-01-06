3b85f24d0bedda9b01f6a251e4fa001714c747ce
HossainAraf <arafat.kd99@gmail.com>
2026-01-06 14:51:44 +0600

    test(Chamber): presence validation

--- test/models/chamber_test.rb
+++ test/models/chamber_test.rb
@@ -4,17 +4,26 @@ class ChamberTest < ActiveSupport::TestCase
   # ---------------------
   # Presence Validations
   # ---------------------
-  # test 'invalid without name' do
-  #   chamber = Chamber.new(category: 'Hospital', address: 'Rubir mor', district_id: 1)
-  #   assert_not chamber.valid?
-  #   assert_includes chamber.errors[:name], "can't be blank"
-  # end
-  test 'invalid without required field' do
-    chamber = Chamber.new()
+  test 'invalid without name' do
+    chamber = Chamber.new(category: 'Hospital', address: 'Rubir mor', district_id: 1)
     assert_not chamber.valid?
     assert_includes chamber.errors[:name], "can't be blank"
+  end
+
+  test 'invalid without category' do
+    chamber = Chamber.new(name: 'City Hospital', address: 'Rubir mor', district_id: 1)
+    assert_not chamber.valid?
     assert_includes chamber.errors[:category], "can't be blank"
+  end
+
+  test 'invalid without address' do
+    chamber = Chamber.new(name: 'City Hospital', category: 'Hospital', district_id: 1)
+    assert_not chamber.valid?
     assert_includes chamber.errors[:address], "can't be blank"
+  end
+  test 'invalid without district_id' do
+    chamber = Chamber.new(name: 'City Hospital', category: 'Hospital', address: 'Rubir mor')
+    assert_not chamber.valid?
     assert_includes chamber.errors[:district_id], "can't be blank"
   end
 end
