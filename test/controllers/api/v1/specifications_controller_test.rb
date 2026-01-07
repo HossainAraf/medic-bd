0df2f4506f37b95f1497da634467dc4e9ecc11e6
HossainAraf <arafat.kd99@gmail.com>
2026-01-07 21:23:32 +0600

    Test hardening: remove old controller tests/fixtures, add cleaned up chambers and specializations tests, update DB/test credentials

--- test/controllers/api/v1/specifications_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class Api::V1::SpecificationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/models/feedback_form_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class FeedbackFormTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
