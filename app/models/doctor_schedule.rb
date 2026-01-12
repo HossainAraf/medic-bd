b806f9173d368b17297aa0b0e1e18bce3a6793a5
HossainAraf <arafat.kd99@gmail.com>
2026-01-12 13:46:30 +0600

    feat(DoctorSchedule): add available_day and slot enum

--- app/models/doctor_schedule.rb
+++ app/models/doctor_schedule.rb
@@ -7,10 +7,26 @@ class DoctorSchedule < ApplicationRecord
   include ::StripWhitespace
 
   accepts_nested_attributes_for :chamber
+  before_save :ensure_chamber
+
+  enum available_day: {
+    sunday: 0,
+    monday: 1,
+    tuesday: 2,
+    wednesday: 3,
+    thursday: 4,
+    friday: 5,
+    saturday: 6
+  }
+
+  enum slot: {
+    morning: 0,
+    afternoon: 1,
+    evening: 2
+  }
 
   validates :contact, presence: true
 
-  before_save :ensure_chamber
 
   private
 
