ba548635d6d4c0de82b77c87704a2ab68ae35ae4
HossainAraf <arafat.kd99@gmail.com>
2026-01-14 12:07:31 +0600

    feat(Doctor): add validation for DoctorSpecialization and strict destoy

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -5,7 +5,7 @@ class Doctor < ApplicationRecord
   include ::StripWhitespace
 
   # Associations
-  has_many :doctor_specializations
+  has_many :doctor_specializations, dependent: :destroy
   has_many :specializations, through: :doctor_specializations
   has_many :doctor_schedules, dependent: :restrict_with_error
   has_many :chambers, through: :doctor_schedules
@@ -18,6 +18,8 @@ class Doctor < ApplicationRecord
   before_save :normalize_display_order
 
   # Validations
+  validates :doctor_specializations, presence: true
+
   validates :display_order, presence: true,
                             numericality: { only_integer: true,
                                             greater_than_or_equal_to: 100_000,
