974ab332b465822c4f8b429fc12bbe1fbd0f42fa
HossainAraf <arafat.kd99@gmail.com>
2026-01-19 00:40:05 +0600

    refactore: update error handing globally form application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,6 +36,19 @@ class ApplicationController < ActionController::API
     nil
   end
 
+rescue_from ActiveRecord::RecordNotFound do |e|
+    render json: { error: e.message }, status: :not_found
+  end
+
+  rescue_from ActiveRecord::RecordInvalid do |e|
+    render json: { errors: e.record.errors.full_messages },
+           status: :unprocessable_entity
+  end
+
+  rescue_from ArgumentError do |e|
+    render json: { error: e.message }, status: :unprocessable_entity
+  end
+
   def extract_token_from_header
     header = request.headers['Authorization']
     log_debug("Authorization Header: #{header}")
