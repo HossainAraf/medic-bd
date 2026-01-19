9b9aa55a157e5a07c55771a4cb0af5408da48a49
HossainAraf <arafat.kd99@gmail.com>
2026-01-19 20:13:44 +0600

    fix(Specialization): specify has_many dependent

--- app/models/specialization.rb
+++ app/models/specialization.rb
@@ -1,5 +1,5 @@
 class Specialization < ApplicationRecord
-  has_many :doctor_specializations
+  has_many :doctor_specializations, dependent: :restrict_with_error
   has_many :doctors, through: :doctor_specializations
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
